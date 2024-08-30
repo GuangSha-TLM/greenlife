b2b038027e1436ee06c2674dc4f59fe5312802e5
Oh...Yeah!!! <614988210@qq.com>
2024-08-30 17:01:57 +0800

    初始化

---
+++ greenlife/src/main/java/com/gsxy/greenlife/GreenLifeApplication.java
@@ -0,0 +1,13 @@
+package com.gsxy.greenlife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenLifeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenLifeApplication.class, args);
+    }
+
+}
---
+++ greenlife/src/main/java/com/gsxy/greenlife/controller/UserController.java
@@ -0,0 +1,127 @@
+package com.gsxy.greenlife.controller;
+
+import com.alibaba.fastjson2.JSONArray;
+import com.gsxy.greenlife.domain.User;
+import com.gsxy.greenlife.domain.bo.UserLoginBo;
+import com.gsxy.greenlife.domain.vo.ResponseVo;
+import com.gsxy.greenlife.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+/**
+ * 用户
+ *
+ * @author Oh...Yeah!!!
+ * @since 2024-03-13 18:25:21
+ */
+@RestController
+@Api(value = "用户版块接口",tags = {"用户版块接口"})
+@RequestMapping(value = "user", produces = "text/plain;charset=UTF-8")
+public class UserController {
+    /**
+     * 服务对象
+     */
+    @Resource
+    private UserService userService;
+
+    /**
+     *  @Auther Oh… Yeah!!! 2024-3-5
+     *  用户注册
+     * @param userLoginBo
+     * @return String.class
+     */
+    @PostMapping("/userReg")
+    @ApiOperation("用户注册")
+    public String userReg(@RequestBody UserLoginBo userLoginBo){
+        if(userLoginBo != null){
+            JSONArray.toJSONString(new ResponseVo("参数为null",null,"0x455"));
+        }
+
+        return JSONArray.toJSONString(userService.userReg(userLoginBo));
+    }
+
+
+    /**
+     *  @Auther Oh… Yeah!!! 2024-3-5
+     *  用户登录
+     * @param userLoginBo
+     * @return
+     */
+    @PostMapping("/login")
+    @ApiOperation("用户登录")
+    public String userLogin(@RequestBody UserLoginBo userLoginBo){
+        if(userLoginBo == null){
+            JSONArray.toJSONString(new ResponseVo("参数为null",null,"0x455"));
+        }
+
+        return JSONArray.toJSONString(userService.userLogin(userLoginBo));
+    }
+
+
+
+    /**
+     * 分页查询
+     *
+     * @param user 筛选条件
+     * @param pageRequest      分页对象
+     * @return 查询结果
+     */
+    @GetMapping("/queryByPage")
+    public ResponseEntity<Page<User>> queryByPage(User user, PageRequest pageRequest) {
+        return ResponseEntity.ok(this.userService.queryByPage(user, pageRequest));
+    }
+
+    /**
+     * 通过主键查询单条数据
+     *
+     * @param id 主键
+     * @return 单条数据
+     */
+    @GetMapping("{id}")
+    public String queryById(@PathVariable("id") Long id) {
+          
+         return JSONArray.toJSONString(new ResponseVo("查询成功",this.userService.queryById(id),"0x200"));
+
+    }
+
+    /**
+     * 新增数据
+     *
+     * @param user 实体
+     * @return 新增结果
+     */
+    @PostMapping("/add")
+    public ResponseEntity<User> add(User user) {
+        return ResponseEntity.ok(this.userService.insert(user));
+    }
+
+    /**
+     * 编辑数据
+     *
+     * @param user 实体
+     * @return 编辑结果
+     */
+    @PutMapping("/edit")
+    public ResponseEntity<User> edit(User user) {
+        return ResponseEntity.ok(this.userService.update(user));
+    }
+
+    /**
+     * 删除数据
+     * @param id 主键
+     * @return 删除是否成功
+     */
+    @DeleteMapping("/deleteById")
+    public ResponseEntity<Boolean> deleteById(Long id) {
+        return ResponseEntity.ok(this.userService.deleteById(id));
+    }
+
+}
+
